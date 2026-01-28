eb19a31705baf517c9a68b0f0d6e4e15b817c99d
MTGPROD <mtgytb45@gmail.com>
2026-01-28 17:32:46 +0100

    Manifestation7

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,16 +13,20 @@ repositories {
     }
 }
 
-tasks.jar {
-    manifest {
-        attributes["Main-Class"] = "org.mtgprod.Main"
-    }
-}
-
 dependencies {
     implementation("com.fazecast:jSerialComm:2.11.4")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.withType<Jar> {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    manifest {
+        attributes["Main-Class"] = "org.mtgprod.Main"
+    }
+
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
 }
 
 tasks.test {
